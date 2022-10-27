a99183a395813d30ab53a4eafce7dbcb8e1f150c
hoshikawa <hoshikawa1309@gmail.com>
2022-10-27 22:05:32 +0900

    add json data method

--- src/main/java/ConditionNotice.java
+++ src/main/java/ConditionNotice.java
@@ -1,6 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConditionNotice {
     public static void main(String[] args){
         TimeTree timeTree = new TimeTree();
-        System.out.println(timeTree.readEvent());
+        // System.out.println(timeTree.readEvent());
+        Map<String, String> event = new HashMap<>();
+        // timeTree.writeEvent();
+
+        System.out.println(timeTree.convertJsonFormat());
     }
 }
+
+
--- src/main/java/TimeTree.java
+++ src/main/java/TimeTree.java
@@ -1,5 +1,4 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.http.HttpClient;
@@ -8,14 +7,14 @@ import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
 import java.util.Properties;
 import java.net.URI;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import org.json.JSONObject;
 
 public class TimeTree {
 
     String accessToken = "";
     String url = "https://timetreeapis.com";
     String calender_id = "";
+    HttpRequest request = null;
 
 
     public TimeTree() {
@@ -28,6 +27,12 @@ public class TimeTree {
             properties.load(input);
             this.accessToken = properties.getProperty("ACCESS_TOKEN");
             this.calender_id = properties.getProperty("CALENDER_ID");
+            this.request = HttpRequest.newBuilder(
+                            URI.create(this.url + "/calendars/" + this.calender_id))
+                    .header("Authorization", "Bearer " + this.accessToken)
+                    .header("Accept", "application/vnd.timetree.v1+json")
+                    .header("Content-Type", "application/json")
+                    .build();
         }catch (IOException e){
             System.out.println(e.getMessage());
             e.getStackTrace();
@@ -37,28 +42,61 @@ public class TimeTree {
     public String readEvent(){
         // 予定を取得する
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(
-                URI.create(this.url + "/calendars/" + this.calender_id))
-                .header("Authorization", "Bearer " + this.accessToken)
-                .header("Accept", "application/vnd.timetree.v1+json")
-                .header("Content-Type", "application/json")
-                .build();
+        String ret_val = "";
         try {
-            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
+            var response = client.send(this.request, HttpResponse.BodyHandlers.ofString());
+            ret_val = response.body();
+
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
-        return this.accessToken;
+        return ret_val;
     }
 
+    /**
+     * 予定を登録する
+     */
     public void writeEvent(){
-        // 予定を登録する
+
+        //
+
         HttpRequest request = HttpRequest.newBuilder(
-                        URI.create(this.url + "/calendars/" + this.calender_id))
+                        URI.create(this.url + "/calendars/" + this.calender_id + "/events"))
                 .header("Authorization", "Bearer " + this.accessToken)
                 .header("Accept", "application/vnd.timetree.v1+json")
                 .header("Content-Type", "application/json")
                 .build();
+
+
+
+    }
+
+    public JSONObject convertJsonFormat(){
+        JSONObject attributes = new JSONObject();
+        attributes.put("category", "schedule");
+        attributes.put("title", "javaです\uD83D\uDE0A");
+        attributes.put("all_day", "true");
+        attributes.put("start_at", "2022-10-04T00:00:00.000Z");
+        attributes.put("start_timezone", "UTC");
+        attributes.put("end_at", "2022-10-04T00:00:00.000Z");
+        attributes.put("end_timezone", "UTC");
+        attributes.put("description", "これはテストです");
+
+        JSONObject relationships_data = new JSONObject();
+        relationships_data.put("id", this.calender_id);
+        relationships_data.put("type", "label");
+
+        JSONObject label = new JSONObject();
+        label.put("label", relationships_data);
+
+        JSONObject event = new JSONObject();
+        event.put("attributes", attributes);
+        event.put("relationships", label);
+
+        JSONObject data = new JSONObject();
+        data.put("data", event);
+
+
+        return data;
     }
 }
